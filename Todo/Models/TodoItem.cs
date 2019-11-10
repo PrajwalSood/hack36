cc75d889f8179012275c3cde60439b20dc1822b8
Prajwal Sood <53113703+PrajwalSood@users.noreply.github.com>
2019-11-10 12:07:20 +0530

    Add files via upload

---
+++ Todo/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace Todo
+{
+	public class TodoItem
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+        public int Votes { get; set; } = 0;
+		public string Name { get; set; }
+		public string Notes { get; set; }
+		public bool Done { get; set; }
+
+        public void Increment()
+        {
+            Votes += 1;
+        }
+	}
+}
+
